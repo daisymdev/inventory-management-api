ad7addec0a800ae18323d34167086598ef3c9abd
Daisy Murillo <daisy21murillo@gmail.com>
2020-11-25 21:24:51 -0700

    product entity

--- src/main/java/entity/Product.java
+++ src/main/java/entity/Product.java
@@ -1,4 +1,61 @@
 package entity;
 
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.Set;
+
+@Entity
 public class Product {
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+
+    @JsonIgnore
+    private Set<Order> orders;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "product_order",
+        joinColumns = @JoinColumns(name = "orderId", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumns(name = "productId", referencedColumnName = "id"))
+    public Set<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(Set<Order> orders) {
+        this.orders = orders;
+    }
+
 }
